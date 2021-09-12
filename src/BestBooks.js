76bc35b50a61c193f99b16abae5c60f580859cad
Faisal-Kushha <fkushha89@hotmail.com>
2021-09-12 17:43:18 +0300

    Done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import './BestBooks.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import "./BestBooks.css";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/ListGroupItem";
 
 class MyFavoriteBooks extends React.Component {
   render() {
-    return(
+    return (
       <Jumbotron>
-        <h1>My Favorite Books</h1>
-        <p>
-          This is a collection of my favorite books
-        </p>
+        <Card style={{ width: "23rem" }}>
+          <Card.Body>
+            <Card.Title>My Favorite Books</Card.Title>
+            <Card.Text>This is a collection of my favorite books</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>The Secret by Rhonda Byrne</ListGroupItem>
+            <ListGroupItem>The Da Vinci Code by Dan Brown.</ListGroupItem>
+            <ListGroupItem>Inferno by Dan Brown</ListGroupItem>
+          </ListGroup>
+        </Card>
       </Jumbotron>
-    )
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// TODO: wrap everything in Auth0
+import React from "react";
+import ReactDOM from "react-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+import App from "./App";
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-i-58qs-d.us.auth0.com"
+    clientId="rYgrAUBElAc5Jr3M9J7VBcpxVsVRfDrh"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
