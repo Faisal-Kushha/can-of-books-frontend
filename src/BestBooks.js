ef061c26b021fb06f53940b1a3f4053a6389ded6
Faisal-Kushha <fkushha89@hotmail.com>
2021-09-13 20:02:30 +0300

    Done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,29 +1,59 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Jumbotron from "react-bootstrap/Jumbotron";
 import "./BestBooks.css";
 import Card from "react-bootstrap/Card";
-import ListGroup from "react-bootstrap/ListGroup";
-import ListGroupItem from "react-bootstrap/ListGroupItem";
+import { withAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+// import Carousel from "react-bootstrap/Carousel";
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      booksArr: [],
+    };
+  }
+
+  componentDidMount = () => {
+    const { user } = this.props.auth0;
+    const email = user.email;
+    axios.get(`http://localhost:3001/books?email=${email}`).then((result) => {
+      this.setState({
+        booksArr: result.data,
+      });
+    });
+  };
   render() {
     return (
-      <Jumbotron>
-        <Card style={{ width: "23rem" }}>
-          <Card.Body>
-            <Card.Title>My Favorite Books</Card.Title>
-            <Card.Text>This is a collection of my favorite books</Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroupItem>The Secret by Rhonda Byrne</ListGroupItem>
-            <ListGroupItem>The Da Vinci Code by Dan Brown.</ListGroupItem>
-            <ListGroupItem>Inferno by Dan Brown</ListGroupItem>
-          </ListGroup>
-        </Card>
-      </Jumbotron>
+      <>
+        <h1>My Favorite Books</h1>
+        <p>This is a collection of my favorite books</p>
+
+        {this.state.booksArr.map((item) => {
+          return (
+            <Card>
+              <Card.Body>
+                <Card.Title>Title: {item.title}</Card.Title>
+                <Card.Text>Description: {item.description}</Card.Text>
+                <Card.Text>Status: {item.status}</Card.Text>
+                <Card.Text>Email: {item.email}</Card.Text>
+              </Card.Body>
+            </Card>
+            // <Carousel>
+            //   <Carousel.Item>
+            //     <Carousel.Caption>
+            //       Title: {item.title}
+            //       Description: {item.description}
+            //       Status: {item.status}
+            //       Email: {item.email}
+            //     </Carousel.Caption>
+            //   </Carousel.Item>
+            // </Carousel>
+          );
+        })}
+      </>
     );
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
